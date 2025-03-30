5944354074862b2525b5a50eeb38d068c1a4164a
Miloblancoo <mblanco6@mail.austral.edu.ar>
2025-03-30 13:10:21 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
-    expense = 23.75
-    money = 100
+
+    gasto = float(input("Ingresar gasto:"))
+    recibido = float(input("Ingresar dinero recibido:"))
+    pesos = (recibido - gasto)
+    print (f"{"gasto:"} {gasto}")
+    print (f"{"recibido:"} {recibido}")
+    print ("")
+    print ("Vuelto")
+    print ("")
+    print (f"{"Pesos:"} {int(pesos)}")
+    print (f"{"Centavos:"} {(int(pesos * 100) %100)}")
+    print ("fin del programa")
+change ()
